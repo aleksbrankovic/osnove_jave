ebd2ab2d57547d3c2d36380e8a4cdbd48b0efe3b
Aleksandra Brankovic <aleksandra.brankovic21@gmail.com>
2022-05-15 19:41:59 +0200

    Domaci 13.05.2022. Zadatak 3 Brisanje iz niza

--- osnove_jave/src/main/java/d13_05_2022/Zadatak3.java
+++ osnove_jave/src/main/java/d13_05_2022/Zadatak3.java
@@ -4,8 +4,6 @@ package d13_05_2022;
 //        elementa koji zeli da izbaci iz niza.
 //        Pre i nakon izbacivanja ispisati niz na ekranu. Ukoliko korisnik
 //        unese poziciju van opsega niza, ispisati poruku o gresci.
-//        Commitujte zadatak i pushujte na github sa porukom Domaci 13.05.2022 Zadatak 3
-//        brisanje iz niza“.
 //        Primer izvrsenja 1:
 //        Niz je: 1,3,5,6,7,1,3,4,2,9
 //        Unesite poziciju za izbacivanje: 5
@@ -41,21 +39,11 @@ public class Zadatak3 {
         System.out.print("Unesite poziciju za izbacivanje: ");
         int pozicija = s.nextInt();
 
-
         if (pozicija < nizBrojeva.size()-1 && pozicija > 0 ) {
             nizBrojeva.remove(pozicija);
             System.out.println("Niz nakon brisanja je: " + nizBrojeva);
         } else {
             System.out.println("Uneli ste nevalidnu poziciju!");
         }
-
-
-
-
-
-
-
-
-
     }
 }
